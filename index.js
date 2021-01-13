71745d2fb80a11ca9530991be0131ac6f66f89cf
Gabriel <furstenheim@gmail.com>
2021-01-13 19:54:18 +0100

    Create docker compose file

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 module.exports = {
 
 }
+
+const dockerCompose = require('docker-compose')
+const dockerComposeFile = require('./lib/docker-compose-file')
+
+main()
+  .then(function (result) {
+    console.log(result)
+    process.exit(0)
+  }, function (err) {
+    console.error('-------')
+    console.error(err)
+    process.exit(1)
+})
+
+async function main () {
+  await setUpStepFunctions()
+}
+
+async function setUpStepFunctions () {
+  const dockerComposeConfig = dockerComposeFile({})
+  const dockerResult = await dockerCompose.upAll({
+    configAsString: dockerComposeConfig,
+    composeOptions: [['--verbose']]
+  })
+
+  console.log(dockerResult)
+}
