bb2cf672c88712b30674ca878a2c9ce7e4118cbd
Gabriel <furstenheim@gmail.com>
2021-02-21 20:33:14 +0100

    Add method to run and clean up

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ module.exports = {
 const dockerCompose = require('docker-compose')
 const dockerComposeFile = require('./lib/docker-compose-file')
 const sam = require('./lib/sam')
+const redis = require('redis')
+const { promisify } = require('util')
+const constants = require('./lib/constants')
+const utils = require('./lib/utils')
+const AWS = require('aws-sdk')
+const AWS_REGION = 'eu-west-1'
+const STEP_FUNCTION_NAME = 'testStepFunction'
+const STEP_FUNCTION_ARN = `arn:aws:states:us-east-1:123456789012:stateMachine:${STEP_FUNCTION_NAME}`
 
 main()
   .then(function (result) {
@@ -14,14 +22,151 @@ main()
     console.error('-------')
     console.error(err)
     process.exit(1)
-})
+  })
 
-async function main () {
-  const result = await sam.runSam()
-  console.log(result)
-  //   await setUpStepFunctions()
+class TestRunner {
+  async setUp () {
+    await sam.runSam()
+    const { endpoint: stepFunctionEndpoint } = await setUpStepFunctions()
+
+    this.stepFunctionClient = new AWS.StepFunctions({
+      endpoint: stepFunctionEndpoint,
+      region: AWS_REGION
+    })
+
+    const redisClient = redis.createClient()
+    this.redisClient = redisClient
+
+    await new Promise(function (resolve) {
+      redisClient.on('connect', resolve)
+      // TODO probably on error reject
+    })
+    const promisifiedGet = promisify(this.redisClient.get).bind(this.redisClient)
+    this.redisGet = async function (key) {
+      const raw = await promisifiedGet(key)
+      return JSON.parse(raw)
+    }
+
+    const promisifiedSet = promisify(this.redisClient.set).bind(this.redisClient)
+
+    this.redisSet = async function (key, obj) {
+      return promisifiedSet(key, JSON.stringify(obj))
+    }
+  }
+
+  async run (callStubs, stepFunctionDefinition, stepFunctionInput, options = {
+    executionTimeout: constants.DEFAULT_EXECUTION_TIMEOUT, executionInterval: constants.DEFAULT_EXECUTION_INTERVAL
+  }) {
+    const executionTimeout = options.executionTimeout
+    const executionInterval = options.executionInterval
+    const stepFunctionClient = this.stepFunctionClient
+    for (const functionKey in callStubs) {
+      // TODO use key
+      const functionExecutions = callStubs[functionKey]
+      await this.redisSet(`stubs:${functionKey}`, functionExecutions)
+    }
+    const fixedDefinition = utils.fixStepFunction(stepFunctionDefinition)
+
+    await stepFunctionClient.createStateMachine({
+      definition: JSON.stringify(fixedDefinition),
+      name: STEP_FUNCTION_NAME,
+      roleArn: 'arn:aws:iam:012345678901:role/DummyRole'
+    }).promise()
+
+    const execution = await stepFunctionClient.startExecution({
+      stateMachineArn: STEP_FUNCTION_ARN,
+      name: new Date().getTime().toString(),
+      input: JSON.stringify(stepFunctionInput)
+    }).promise()
+
+    let stillRunning = true
+    let executionError = null
+    setTimeout(function () {
+      stillRunning = false
+      executionError = new Error('Step function not finished withing requested timeout')
+    }, executionTimeout)
+
+    let stepFunctionResult
+    while (stillRunning) {
+      stepFunctionResult = await stepFunctionClient.describeExecution({ executionArn: execution.executionArn}).promise()
+      if (stepFunctionResult.status === 'RUNNING') {
+        await utils.waitFor(executionInterval)
+      } else {
+        stillRunning = false
+      }
+    }
+
+    if (executionError !== null) {
+      throw executionError
+    }
+
+    const executionHistory = await stepFunctionClient.getExecutionHistory({executionArn: execution.executionArn}).promise()
+
+    const redisClient = this.redisClient
+    const executionKeys = await new Promise(function (resolve, reject) {
+      redisClient.keys('executions:*', function (err, keys) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(keys)
+      })
+    })
+    const redisGet = this.redisGet
+    const executions = await Promise.all(executionKeys.map(async function (key) {
+      const execution = redisGet(key)
+      const functionName = key.substr('executions:'.length)
+      return {
+        functionName,
+        execution
+      }
+    }))
+
+    return {
+      executions,
+      stepFunctionExecution: stepFunctionResult,
+      stepFunctionHistory: executionHistory
+    }
+  }
+
+  async cleanUp () {
+    const redisClient = this.redisClient
+    const keys = await new Promise(function (resolve, reject) {
+      redisClient.keys('*', function (err, keys) {
+        if (err) {
+          return reject(err)
+        }
+        resolve(keys)
+      })
+    })
+    const del = promisify(redisClient.del).bind(redisClient)
+    for (const key of keys) {
+      await del(key)
+    }
+
+    const { executions } = await this.stepFunctionClient.listExecutions({ stateMachineArn: STEP_FUNCTION_ARN }).promise()
+    for (const execution of executions) {
+      await this.stepFunctionClient.stopExecution({ executionArn: execution.executionArn }).promise()
+    }
+    await this.stepFunctionClient.deleteStateMachine({ stateMachineArn: STEP_FUNCTION_ARN }).promise()
+  }
+
+  async tearDown () {
+    // TODO close docker and sam
+  }
 }
 
+async function main () {
+  const testRunner = new TestRunner()
+  await testRunner.setUp()
+  const result = testRunner.run(
+    {},
+  )
+}
+
+/**
+ *
+ * @returns {Promise<{endpoint: number}>}
+ */
 async function setUpStepFunctions () {
   const dockerComposeConfig = dockerComposeFile({})
   const dockerResult = await dockerCompose.upAll({
@@ -30,4 +175,8 @@ async function setUpStepFunctions () {
   })
 
   console.log(dockerResult)
+  // TODO accept endpoint as parameter
+  return {
+    endpoint: `http://localhost:${constants.DEFAULT_STEP_FUNCTIONS_PORT}`
+  }
 }
